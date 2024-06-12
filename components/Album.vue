34f20ce5b44f2c1f7384ed8865d3defbf67ceca0
Zhassulan Serikuly <zhass@MacBook-Air-Zhassulan.local>
2024-06-13 01:08:15 +0500

    album

--- components/Album.vue
+++ components/Album.vue
@@ -1,117 +1,44 @@
 <template>
-  <VueFlux
-   :options="options"
-   :rscs="rscs"
-   :transitions="transitions"
+  <Swiper
+    :modules="[Autoplay, Navigation, Pagination, EffectFade]"
+    :spaceBetween="30"
+    :autoplay="autoplay"
+    :navigation="true"
+    :loop="true"
+    effect="fade"
+    class="album"
   >
-   <template #preloader="preloaderProps">
-      <FluxPreloader v-bind="preloaderProps" />
-   </template>
-
-   <template #controls="controlsProps">
-      <FluxControls v-bind="controlsProps" />
-   </template>
-</VueFlux>
+    <SwiperSlide v-for="(image, index) in images" :key="index">
+      <img :src="image" :alt="`Slide ${index + 1}`" />
+    </SwiperSlide>
+  </Swiper>
 </template>
 
 <script setup>
-import { VueFlux, FluxPreloader, FluxControls, Img, Fade, Kenburn } from "vue-flux";
+import { ref } from "vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay, Navigation, Pagination, EffectFade } from "swiper";
 import image1 from "@/assets/images/IMG_0688.jpeg";
 import image2 from "@/assets/images/IMG_0689.jpeg";
 import image3 from "@/assets/images/IMG_0690.jpeg";
 import image4 from "@/assets/images/IMG_0691.jpeg";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-const options = reactive({
-	allowFullscreen: false,
-	allowToSkipTransition: false,
-	autohideTime: 2000,
-	autoplay: true,
-	bindKeys: false,
-	delay: 2000,
-	enableGestures: false,
-	infinite: true,
-	lazyLoad: true,
-	lazyLoadAfter: 3,
-});
-const rscs = shallowReactive([
-	new Img(image1),
-	new Img(image2),
-	new Img(image3),
-	new Img(image4)
-]);
-const transitions = shallowReactive([
-  Fade,
-  Kenburn,
-]);
+const images = ref([image1, image2, image3, image4]);
+const autoplay = {
+  delay: 3500,
+  disableOnInteraction: false,
+};
 </script>
 
 <style lang="scss">
 @use "@/assets/styles/mixins" as *;
 
-.vue-flux {
-  position: relative;
+.album {
   @include fullpage;
   @include border_rad;
-  .flux-transition {
-    position: absolute;
-  }
-  & > .flux-image,
-  .complements {
-    @include origin;
-  }
-  .complements {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    z-index: 45;
-    .remainder {
-      flex-basis: 50%;
-    }
-  }
-  .flux-controls {
-    @include fullpage;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 16px;
-    .flux-button {
-      padding: 0;
-      width: 6%;
-      min-width: 36px;
-      min-height: 36px;
-      max-width: 48px;
-      max-height: 48px;
-      border: 0;
-      cursor: pointer;
-      background-color: transparent;
-      &:hover {
-        > svg {
-          line, polyline {
-            stroke: yellow;
-          }
-          rect, polygon {
-            fill: yellow;
-          }
-        }
-      }
-      > svg {
-        width: 100%;
-        > circle {
-          fill: rgba(0, 0, 0, 0.7);
-        }
-        line, polyline, rect, polygon {
-          stroke-linecap: round;
-          stroke-linejoin: round;
-          stroke: var(--clr-white);
-          stroke-width: 14;
-          fill: none;
-        }
-        rect, polygon {
-          fill: var(--clr-white);
-          stroke-width: 0;
-        }
-      }
-    }
-  }
 }
 </style>

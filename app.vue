65fee5c6c28d78a1849611a7e5c8bb0d352f148a
Zhassulan Serikuly <zhass@MacBook-Air-Zhassulan.local>
2024-06-13 13:54:56 +0500

    motion

--- app.vue
+++ app.vue
@@ -1,28 +1,30 @@
 <template>
   <main>
     <img src="@/assets/images/background.jpeg" alt="Background" class="background" />
-    <Player />
-    <section-home />
-    <Section>
-      <section-about />
-    </Section>
-    <Border />
-    <Section>
-      <section-time />
-    </Section>
-    <Album />
-    <Section>
-      <section-location />
-    </Section>
-    <Section>
-      <section-host />
-    </Section>
-    <img src="@/assets/images/bukety.jpg" alt="Wedding flower" />
-    <Section>
-      <section-countdown />
-    </Section>
-    <Section>
-      <section-form />
-    </Section>
+    <MotionGroup preset="popVisible" :duration="300">
+      <Player />
+      <section-home />
+      <Section>
+        <section-about />
+      </Section>
+      <Border />
+      <Section>
+        <section-time />
+      </Section>
+      <Album />
+      <Section>
+        <section-location />
+      </Section>
+      <Section>
+        <section-host />
+      </Section>
+      <img src="@/assets/images/bukety.jpg" alt="Wedding flower" />
+      <Section>
+        <section-countdown />
+      </Section>
+      <Section>
+        <section-form />
+      </Section>
+    </MotionGroup>
   </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["@/assets/styles/main.css"]
+  css: ["@/assets/styles/main.css"],
+  modules: ['@vueuse/motion/nuxt'],
+  runtimeConfig: {
+    public: {
+      motion: {
+        directives: {
+          'pop-bottom': {
+            initial: {
+              scale: 0,
+              opacity: 0,
+              y: 100,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              y: 0,
+            }
+          }
+        }
+      }
+    }
+  }
 });
